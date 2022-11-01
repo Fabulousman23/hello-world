92773bb51373a8949de2470a469c5dc30363afda
Fabulousman23 <andrew.makiev@gmail.com>
2022-11-01 15:11:42 -0400

    eidt

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, Text, TextInput, Button, Alert } from 'react-native';
-import Chat from '/components/Chat';
-import Start from '/components/Start';
+import Screen1 from './components/Screen1';
+import Screen2 from './components/Screen2';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+
 
 const Stack = createStackNavigator();
 
@@ -12,7 +13,18 @@ export default class App extends React.Component {
   render() {
     return (
       <NavigationContainer>
-        {/* rest of the code */}
+        <Stack.Navigator
+          initialRouteName="Screen1"
+        >
+          <Stack.Screen
+            name="Screen1"
+            component={Screen1}
+          />
+          <Stack.Screen
+            name="Screen2"
+            component={Screen2}
+          />
+        </Stack.Navigator>
       </NavigationContainer>
     );
   }

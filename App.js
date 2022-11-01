8747ad8423cb32c5204eab8543277d4f7ccd8c6d
Fabulousman23 <andrew.makiev@gmail.com>
2022-11-01 14:09:34 -0400

    add react-navigation

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, View, Text, TextInput, Button, Alert } from 'react-native';
+//import Chat from '/components/Chat';
+//import Start from '/components/Start';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: '' };
+  }
+
+  alertMyText(input = []) {
+    Alert.alert(input.text);
+  }
+
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center' }}>
+        <TextInput
+          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+          onChangeText={(text) => this.setState({ text })}
+          value={this.state.text}
+          placeholder='Type here ...'
+        />
+        <Text>You wrote: {this.state.text}</Text>
+        <Button
+          onPress={() => {
+            this.alertMyText({ text: this.state.text });
+          }}
+          title="Press Me"
+        />
+
+      </View>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
